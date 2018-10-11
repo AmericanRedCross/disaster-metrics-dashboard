dbc35ffca287f0267461195a1cb533803ed001bb
danbjoseph <danbjoseph@gmail.com>
2018-10-10 21:15:49 -0400

    img folder reorg

--- js/main-lombok.js
+++ js/main-lombok.js
@@ -36,7 +36,7 @@ function vizIt(data, tabletop) {
   for(i = 0; i < pageData.length; i++) {
     if(pageData[i].key == "secondary-figure") {
       var figureHtml = '<div class="col-xs-3">' +
-          '<img class="pull-left" src="../img/' + pageData[i].img + '" height="60px">' +
+          '<img class="pull-left" src="../img/secondary-icons/' + pageData[i].img + '" height="60px">' +
           '<div><span class="secondary-figure_number">' + pageData[i].value + '</span><br>' +
             '<span class="secondary-figure_name">' + pageData[i].label + '</span>' +
           '</div>' +
